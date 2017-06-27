349ada37e93d57853c3962385de29b646e67d56e
Alan Casas <alancasas@MacBook-Pro-de-Alan.local>
2017-06-27 07:53:49 +0200

    Cambio el archivo de app.component.html que tenia mal Creo un nuevo componente que será la barra de navegación de la pagina Creo el archivo de rutas que nos hace las redireccione al poner rutas en la dirección http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
+import { APP_ROUTING } from './app.routes';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SearchComponent
+    SearchComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
